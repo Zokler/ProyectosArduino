85f73150f06ad08d16c638c3cf8f87d9bf8c758b
Jonathan <jonavaz18@gmail.com>
2019-05-17 00:22:46 -0500

    proyecto

--- SegundoParcial/pruebabt/pruebabt.ino
+++ SegundoParcial/pruebabt/pruebabt.ino
@@ -1,9 +1,18 @@
-int led13=13;
+//variables motores:
+int in1 = 8; //pin 8
+int in2 = 9; //pin 9
+//int enA = 10; //pin 10
+int in3 = 10;
+int in4 = 11;
 int estado=0;
 
 void setup(){
   Serial.begin(9600);
-  pinMode(led13,OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
 }
 
 void loop(){
@@ -12,9 +21,35 @@ void loop(){
  Serial.print(estado + " ");
  }
  if (estado =='1'){
-   digitalWrite(led13,HIGH);
+  //AVANZAR
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH); //combinacion para avanzar
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH); //combinacion para avanzar
+   
   }
 if(estado=='2'){
-   digitalWrite(led13,LOW);
+  //ATRAS
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW); //combinacion para avanzar
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW); //combinacion para avanzar
   }
+  if(estado=='3'){
+   //DERECHA
+   
+  }
+  if(estado=='4'){
+    //IZQUIERDA
+   
+  }
+  if(estado=='6'){
+    //PARAR
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW); //combinacion para avanzar
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW); //combinacion para avanzar
+  }
+
+  
 }  
